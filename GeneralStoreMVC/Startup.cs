a9da5c8d486ce75252fbf223ee02044017e9dad7
Matthew Russell <mrussell5361@gmail.com>
2022-05-23 15:40:25 -0400

    Added Customer Model And Customer Index View

--- GeneralStoreMVC/Startup.cs
+++ GeneralStoreMVC/Startup.cs
@@ -1,6 +1,8 @@
+using GeneralStoreMVC.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace GeneralStoreMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GeneralStoreDBContext>(
+                options => options.UseSqlServer(Configuration["ConnectionStrings:GSLocal"]));
             services.AddControllersWithViews();
         }
 
